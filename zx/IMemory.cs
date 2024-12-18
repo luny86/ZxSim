11b56f4f60c7ca5a0675b67b593e54e0574b0769
luny <remakes@luny.co.uk>
2024-12-18 13:39:23 +0000

    Start of sub projects.

---
+++ zx/IMemory.cs
@@ -0,0 +1,30 @@
+
+namespace GameEditorLib.zx
+{
+    /// <summary>
+    /// Describes the memory of a snapshot.
+    /// </summary>
+    public interface IMemory
+    {
+        /// <summary>
+        /// Name of snapshot loaded.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Start address (Spectrum memory map)
+        /// of first byte.
+        /// </summary>
+        int Start { get; }
+
+        /// <summary>
+        /// Number of bytes in total.
+        /// </summary>
+        int Length { get; }
+
+        /// <summary>
+        /// Main indexer.
+        /// </summary>
+        byte[] this[int index] { get; }
+    }
+}
