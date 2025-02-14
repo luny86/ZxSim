7c7b3faa97a764450a234e0a2033bea9f9de7158
kevan <remakes@luny.co.uk>
2025-02-14 13:00:27 +0000

    Added some static versions of the available instances from the build pool.

---
+++ ZX.Drawing/ClassNames.cs
@@ -0,0 +1,12 @@
+
+namespace ZX.Drawing
+{
+    /// <summary>
+    /// Names of each class that is available on the creator pool as
+    /// dependencies.
+    /// </summary>
+    public static class ClassNames
+    {
+        public static string Screen = "ZX.Drawing.Screen";
+    }
+}
---
+++ ZX.Game/ClassNames.cs
@@ -0,0 +1,15 @@
+
+namespace ZX.Game
+{
+    /// <summary>
+    /// Names of each class that is available on the creator pool as
+    /// dependencies.
+    /// </summary>
+
+    public static class ClassNames
+    {
+        public static string Flags = "ZX.Game.Flags";
+        public static string Factory = "ZX.Game.Factory";
+        public static string GameProvider = "ZX.Game.GameProvider";
+    }
+}
