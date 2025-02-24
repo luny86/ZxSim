8905d80f1d8425b49f5948ac95dda164e8da4c5a
kevan <remakes@luny.co.uk>
2025-02-24 16:56:56 +0000

    Extended Rectangle to be empty and tests of In Rect.

--- ZX.Util/Rectangle.cs
+++ ZX.Util/Rectangle.cs
@@ -15,6 +15,10 @@ namespace ZX.Util
             H = h;
         }
 
+        public static Rectangle Empty => new Rectangle(0,0,0,0);
+
+        public bool IsEmpty => X==0 && Y==0 && W==0 && H==0;
+        
         public int X { get; }
         public int Y { get; }
         public int W { get; }
@@ -24,5 +28,12 @@ namespace ZX.Util
         public int Right => X+W;
         public int Top => Y;
         public int Bottom => Y+H;
+
+        public bool InRect(int x, int y)
+        {
+            return (x >= Left && x < Right) &&
+                    (y >= Top && y < Bottom);
+        }
+
     }
 }
