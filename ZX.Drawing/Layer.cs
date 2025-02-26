86da9f5432328410db425307d4d622cf0424219d
kevan <remakes@luny.co.uk>
2025-02-26 17:05:59 +0000

    Layer comments added.

--- ZX.Drawing/Layer.cs
+++ ZX.Drawing/Layer.cs
@@ -8,6 +8,9 @@ namespace ZX.Drawing
     /// to the main screen. Override <see cref="Update"/> to handle
     /// how things are drawn etc.
     /// </summary>
+    /// <remarks>
+    /// Comparisons are done using the Z order value.
+    /// </remarks>
     public abstract class Layer : ILayer, IEquatable<ILayer>, IComparable<ILayer>, IComparable
     {
         public Layer(string name, ISurface surface, int z)
@@ -19,16 +22,37 @@ namespace ZX.Drawing
             Y = 0;
         }
 
+        /// <summary>
+        /// Name of layer.
+        /// </summary>
         public string Name { get; }
 
+        /// <summary>
+        /// Gets the surface that holds the image
+        /// of the layer.
+        /// </summary>
+        /// <remarks>
+        /// THis is the surface that is finally blended
+        /// onto the main surface and should be used
+        /// to draw to when updating the image of the
+        /// layer.
+        /// </remarks>
         protected ISurface Surface { get; }
 
         public int Z { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
 
+        /// <summary>
+        /// Called when the image needs redrawing or changing.
+        /// </summary>
         public abstract void Update();
 
+        /// <summary>
+        /// Call when the layer needs to be displayed
+        /// onto a main surface such as the <see cref="IScreen"/> 
+        /// </summary>
+        /// <param name="to">Surface to blend to.</param>
         public void Blend(ISurface to)
         {
             Surface.Blend(to, X, Y);
