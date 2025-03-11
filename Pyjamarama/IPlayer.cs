9612cb0e723c4831e2967ea1f05cd468c6a7978e
kevan <remakes@luny.co.uk>
2025-03-11 17:25:48 +0000

    Missed file for wally controller

--- Pyjamarama/IPlayer.cs
+++ Pyjamarama/IPlayer.cs
@@ -7,10 +7,12 @@ namespace Pyjamarama
     {
         bool Disabled { get; }
 
+        bool Visible { get; set; }
+
         bool IsDead { get; }
         
-        Point Position { get; }
+        Point Position { get; set; }
 
-        bool JustPickedUp { get; }
+        bool JustPickedUp { get; set; }
     }
 }
